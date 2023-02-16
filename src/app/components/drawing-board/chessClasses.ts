53e473d639a3d42ca65d7890d9b57d734a62412f
hei <izzie0082005@gmail.com>
2023-02-16 04:16:14 +0000

    chessBoardSetUp

--- src/app/components/drawing-board/chessClasses.ts
+++ src/app/components/drawing-board/chessClasses.ts
@@ -1,3 +1,89 @@
-export class ChessPiece{
+interface hGrid {
+    sizeX: number
+    sizeY: number
+}
+
+export class ChessPiece {
+    name: any
+    type: any
+    owner: any
+    health: any
+    status: any
+}
+
+export class GridBoard {
+    boardSize = 8;
+    gridArray: any = []
+    whitePlayerPiece: any = []
+    blackPlayerPiece: any = []
+    pieceOrder = ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'];
+    pieceElementSelector: any
+    pieceDataSelector: any
+
+    constructor() {
+        this.whitePlayerPiece = this.initializePiece(2, 8)
+        this.placeChessOnGrid(this.whitePlayerPiece, 0, 1);
+
+        this.blackPlayerPiece = this.initializePiece(2, 8).reverse();
+        this.placeChessOnGrid(this.blackPlayerPiece, 48, 1);
+    }
+    // setup
+    initializeGrid() {
+        let outputGridArray: any = []
+        for (let i = 0; i < this.boardSize; i++) {
+            for (let j = 0; j < this.boardSize; j++) {
+                outputGridArray[outputGridArray.length] = { key: i * this.boardSize + j, value: '' };
+            }
+        }
+        return outputGridArray
+    }
+
+    initializePiece(sizeX: number, sizeY: number) {
+        let outputGridArray: any = [];
+        for (let i = 0; i < sizeX * sizeY; i++) {
+            outputGridArray[i] = this.pieceOrder[i];
+        }
+        return outputGridArray
+    }
+
+    placeChessOnGrid(targetChessArray: string[], from: any, direction: number) {
+        let targetChessArrayCount = 0;
+        for (let i = 0; i < targetChessArray.length; i = i + direction) {
+            // this.gridArray[from+i] = targetChessArray[targetChessArrayCount];
+            this.gridArray[from + i] = { key: from + i, value: targetChessArray[targetChessArrayCount] };
+            targetChessArrayCount++
+        }
+    }
     
+    // controol
+    selectPiece(selectElement: any, selectData: any) {
+        if (selectElement == this.pieceElementSelector) {
+            this.pieceElementSelector.style.backgroundColor = '#FF0000';
+            this.pieceElementSelector = undefined;
+            this.pieceDataSelector = undefined;
+        }
+        else if (this.pieceElementSelector == undefined) {
+            selectElement.style.backgroundColor = '#FFFFFF';
+            this.pieceElementSelector = selectElement;
+            this.pieceDataSelector = selectData;
+        }
+        else {
+            this.moveChessPiece(this.gridArray[this.pieceDataSelector.key], this.gridArray[selectData.key]);
+            this.pieceElementSelector.style.backgroundColor = '#FF0000';
+            this.pieceElementSelector = undefined;
+            this.pieceDataSelector = undefined;
+        }
+    }
+    moveChessPiece(from: any, to: any) {
+        this.checkValidMove() ? this.swapping(from, to) : alert('falsMove');
+    }
+
+    swapping(from: any, to: any) {
+        let pieceDataHolder = this.gridArray[to.key];
+        this.gridArray[to.key] = this.gridArray[from.key];
+        this.gridArray[from.key] = pieceDataHolder;
+    }
+    checkValidMove() {
+        return true
+    }
 }
--- src/app/components/drawing-board/drawing-board.component.ts
+++ src/app/components/drawing-board/drawing-board.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { CdkDragDrop, moveItemInArray } from '@angular/cdk/drag-drop';
+import { GridBoard } from '../drawing-board/chessClasses'
 interface hGrid {
   sizeX: number
   sizeY: number
@@ -15,55 +16,83 @@ export class DrawingBoardComponent implements OnInit {
   gridArray: any = []
   whitePlayerPiece: any = []
   blackPlayerPiece: any = []
-  pieceOrder = ['r','n','b','r','q','k','b','n','r','p','p','p','p','p','p','p','p','p'];
-  pawnPieceOrder = ['p','p','p','p','p','p','p','p','p'];
+  pieceOrder = ['r', 'n', 'b', 'q', 'k', 'b', 'n', 'r', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'];
+  pawnPieceOrder = ['p', 'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'];
+
+  pieceElementSelector: any
+  pieceDataSelector: any
 
   constructor() {
     let testGrid: hGrid = { sizeX: this.boardSize, sizeY: this.boardSize };
     this.gridArray = this.initializeGrid(testGrid)
-    this.whitePlayerPiece = this.initializePiece(2, 8, '')
-    this.blackPlayerPiece = this.initializePiece(2, 8, '')
-    this.blackPlayerPiece = this.blackPlayerPiece.reverse();
-    this.placeChessOnGrid(this.whitePlayerPiece,0,1);
-    this.placeChessOnGrid(this.blackPlayerPiece,56,1);
-    console.log(this.gridArray)
+
+    this.whitePlayerPiece = this.initializePiece(2, 8)
+    this.placeChessOnGrid(this.whitePlayerPiece, 0, 1);
+
+    this.blackPlayerPiece = this.initializePiece(2, 8).reverse();
+    this.placeChessOnGrid(this.blackPlayerPiece, 48, 1);
+
   }
 
   initializeGrid(testGrid: hGrid) {
     let outputGridArray: any = []
     for (let i = 0; i < testGrid.sizeY; i++) {
       for (let j = 0; j < testGrid.sizeX; j++) {
-        outputGridArray[outputGridArray.length] = i * this.boardSize + j;
+        outputGridArray[outputGridArray.length] = { key: i * this.boardSize + j, value: '' };
       }
     }
     return outputGridArray
   }
 
-  initializePiece(sizeX: number, sizeY: number, targetObject: any) {
+  initializePiece(sizeX: number, sizeY: number) {
     let outputGridArray: any = [];
-    for (let i = 0; i < sizeX*sizeY; i++) {
-      outputGridArray[i] = this.pieceOrder[i]; 
+    for (let i = 0; i < sizeX * sizeY; i++) {
+      outputGridArray[i] = this.pieceOrder[i];
     }
     return outputGridArray
   }
-  placeChessOnGrid(targetChessArray:string[],from:any,direction:number){
-    for (let i = from; i < targetChessArray.length; i+direction) {
-      this.gridArray[i] = targetChessArray[i];
+
+
+  placeChessOnGrid(targetChessArray: string[], from: any, direction: number) {
+    let targetChessArrayCount = 0;
+    for (let i = 0; i < targetChessArray.length; i = i + direction) {
+      // this.gridArray[from+i] = targetChessArray[targetChessArrayCount];
+      this.gridArray[from + i] = { key: from + i, value: targetChessArray[targetChessArrayCount] };
+      targetChessArrayCount++
     }
   }
 
-  rotateChesOrder(targetChessArray:string[]){
-    targetChessArray.reverse();
+  selectPiece(selectElement: any, selectData: any) {
+    if (selectElement == this.pieceElementSelector) {
+      this.pieceElementSelector.style.backgroundColor = '#FF0000';
+      this.pieceElementSelector = undefined;
+      this.pieceDataSelector = undefined;
+    }
+    else if (this.pieceElementSelector == undefined) {
+      selectElement.style.backgroundColor = '#FFFFFF';
+      this.pieceElementSelector = selectElement;
+      this.pieceDataSelector = selectData;
+    }
+    else {
+      this.moveChessPiece(this.gridArray[this.pieceDataSelector.key], this.gridArray[selectData.key]);
+      this.pieceElementSelector.style.backgroundColor = '#FF0000';
+      this.pieceElementSelector = undefined;
+      this.pieceDataSelector = undefined;
+    }
+  }
+  moveChessPiece(from: any, to: any) {
+    this.checkValidMove() ? this.swapping(from,to) : alert('falsMove');
+  }
+  
+  swapping(from: any, to: any) {
+    let pieceDataHolder = this.gridArray[to.key];
+    this.gridArray[to.key] = this.gridArray[from.key];
+    this.gridArray[from.key] = pieceDataHolder;
   }
 
-
-
-
-
-
-
-
-  
+  checkValidMove() {
+    return true
+  }
 
   ngOnInit() { }
 
